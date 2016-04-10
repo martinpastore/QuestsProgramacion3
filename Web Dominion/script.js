c3d36b6b9ba545f180df9eb7d202719407112b0d
Bocha <Bocha>
2016-04-10 18:57:36 -0300

    finish quest

---
+++ Web Dominion/script.js
@@ -0,0 +1,17 @@
+function copyText(){
+	var texto1 = document.getElementById("txtUno").value;
+	var texto2 = document.getElementById("txtDos").value;
+
+
+	if(document.getElementById("txtDos").value == ''){
+		$("#txtDos").val(texto1);
+	}
+	else if(document.getElementById("txtUno").value == ''){
+		$("#txtUno").val(texto2);
+	}
+
+	/*
+	if(texto1 = "" && texto2 =){
+		document.write("Please, write something");
+	}*/
+}	
