08be57a65ebbc9f49be237ea3aaa8a25c8097e14
unknown <martin.pastore@AR-IT02995.globant.com>
2016-04-28 12:25:00 -0300

    hash algorithm

--- Hash Is/script.js
+++ Hash Is/script.js
@@ -1,15 +1,26 @@
 var hash = function(texto){
 	var mayus="ABCDEFGHYJKLMNÑOPQRSTUVWXYZ"
 	var minus="abcdefghyjklmnñopqrstuvwxyz";
-	var cant = texto.length;
 	var char = [];
+	var textoRet = "";
+	var extra = 0;
+	var extraChar = "!#$%&(=)*¬@▼ô";
+	var randomChar = "";
 	for(var i = 0; i < texto.length; i++){
 		char[i] = texto.charAt(i);
 		if(mayus.indexOf(char[i],0)!=-1){
-			char[i].toLowerCase();
+			char[i] = char[i].toLowerCase();
 		}else{
-			char[i].toUpperCase();
-		}
-		console.log(char[i]);
-	}
+			char[i] = char[i].toUpperCase();
+		}	
+		var rnum = Math.floor(Math.random() * extraChar.length);
+		randomChar = extraChar.substring(rnum,rnum+1);				
+			
+		extra = Math.round(Math.random() * 9);
+		textoRet += char[i] + extra + randomChar; 
+	}	
+	console.log(textoRet);
+}
+
+var hashOut = function(texto){
 }
